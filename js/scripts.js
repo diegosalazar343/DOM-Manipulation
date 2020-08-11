14f745ddcd5a41d7d47bb6c8b62ff5859981cd84
diegosalazar343 <salazardiego343@gmail.com>
2020-08-11 15:32:34 -0700

    add css,js,html page, ul id tags, end at remove
    Co-authored-by: Ophelia Swen <ophelia.swen@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+  $("button#hello").click(function () {
+    $("ul#user").children("li").first().click(function(){
+      alert('hi');
+    $("ul#webpage").children("li").first().click(function() {
+      alert('hi');
+    });
+
+  $("button#goodbye").click(function () {
+    $("ul#user").children("li").first().click(function () {
+      alert('goodbye');
+    $("ul#webpage").children("li").first().click(function () {
+      alert('goodbye');
+  });
+
+  $("button#stop").click(function () {
+    $("ul#user").children("li").first().click(function () {
+      alert('stop copying me');
+    $("ul#webpage").children("li").first().click(function () {
+        alert('stop copying me');
+ });
+});
